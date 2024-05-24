4f3c23b143ff03e3e6e8cfff3dcf887f9247b558
Juanjo Corbalán <juanjocorbalan@gmail.com>
2024-05-24 10:30:22 +0200

    Initial Commit

---
+++ AutoScrollableSectionedScrollView/AutoScrollableSectionedScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScrollableSectionedScrollViewApp.swift
+//  AutoScrollableSectionedScrollView
+//
+//  Created by Juanjo Corbalan on 24/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AutoScrollableSectionedScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
